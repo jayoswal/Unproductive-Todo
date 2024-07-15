1151195d9fdf68d4c5ae1d33dd94c22dc03a0fa2
jayoswal <jayumeshoswal2001@gmail.com>
2024-07-15 16:07:40 +0530

    Adding new project files

---
+++ src/main/java/com/jayoswal/unproductive_todo/UnproductiveTodoApplication.java
@@ -0,0 +1,13 @@
+package com.jayoswal.unproductive_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnproductiveTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnproductiveTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jayoswal/unproductive_todo/UnproductiveTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayoswal.unproductive_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnproductiveTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
